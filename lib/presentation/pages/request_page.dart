7611c72bcdb6ae747d66a7b944e11fa57f3de17b
Hipz <vgtmaxwell@gmail.com>
2025-02-06 20:44:08 +0700

    update view

--- lib/presentation/pages/request_page.dart
+++ lib/presentation/pages/request_page.dart
@@ -5,7 +5,7 @@ class RequestPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(child: Text('Request Page')),
     );
   }
